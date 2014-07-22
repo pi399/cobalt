708c1d6405c9db99987827d93873a240d3b45b32
pi399 <ianpdw@gmail.com>
2014-07-22 02:58:34 -0500

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -1,9 +1,5 @@
 --[[conf.lua
-	Copyright 2014 Ian DeWalt
-	This is a project intended solely to create fully-fledged collision detection
-	from scratch. It has dragged on for too long, but I want to do it. Some
-	assistance comes from Brett Sears.
-]]
+Copyright 2014 Ian DeWalt]]
 
 function love.conf(t)
 	t.identity="./"
