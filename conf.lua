f6502c6f1c8a7df895c1d1bff1ef120196d346b4
pi399 <ianpdw@gmail.com>
2014-07-22 02:48:39 -0500

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -3,10 +3,6 @@
 	This is a project intended solely to create fully-fledged collision detection
 	from scratch. It has dragged on for too long, but I want to do it. Some
 	assistance comes from Brett Sears.
-	
-	Things that may be added:
-	Making velocity on diagonals the same as on the axes
-	Sprites
 ]]
 
 function love.conf(t)
