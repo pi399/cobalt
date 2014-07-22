c7ac979f5c61f4c3f54a37d363c5e3d5b6a3d8c2
pi399 <ianpdw@gmail.com>
2014-07-22 03:05:06 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,14 +5,10 @@ World=require "world"		local World=World
 local love=love
 local ipairs=ipairs
 
-local keys, debug
+local keys,debug
 local world
-
-local oxygenmono, dayposterblack
-
-local function round(num)
-	return math.floor(num + 0.5)
-end
+local oxygenmono,dayposterblack
+local function round(num) return math.floor(num+0.5) end
 
 function love.load()
 	keys={["up"]="w",["down"]="s",["left"]="a",["right"]="d",
@@ -20,31 +16,19 @@ function love.load()
 	debug=false
 	
 	world=World:loadFile("resources/worlds/testworld")
+	world:initializeCollisions()
+	world:basicSprites()
 	
 	love.graphics.setBackgroundColor(255,255,255)
-	love.graphics.setColor(0,0,0)
-	
-	love.graphics.setCanvas(world.player.sprite)
-	love.graphics.rectangle("fill",0,0,world.player.xl,world.player.yl)
-	love.graphics.setCanvas(world.object1.sprite)
-	love.graphics.rectangle("fill",0,0,world.object1.xl,world.object1.yl)
-	love.graphics.setCanvas(world.object2.sprite)
-	love.graphics.rectangle("fill",0,0,world.object2.xl,world.object2.yl)
-	love.graphics.setCanvas()
-	
-	world:initializeCollisions()
-	
 	love.window.setTitle("LÖVE Collisions v.0.3")
-	oxygenmono, dayposterblack = love.graphics.newFont("resources/fonts/oxygenmono.otf"),
+	oxygenmono,dayposterblack=love.graphics.newFont("resources/fonts/oxygenmono.otf"),
 		love.graphics.newFont("resources/fonts/dayposterblack.ttf",60)
-	love.graphics.setFont(oxygenmono)
 end
 
 function love.update(dt)
 	for i,ma in ipairs(world) do
 		ma:update(dt)
-	end
-end
+end end
 
 function pausedupdate() end
 
@@ -54,13 +38,13 @@ function love.draw()
 	end
 	
 	if debug then
-		love.graphics.print("world name: ".. world.name
+		love.graphics.setColor(0,0,0) love.graphics.setFont(oxygenmono)
+		love.graphics.print("world name: "..world.name
 		.."\nx: "..round(world.player.x)..", y: "..round(world.player.y).."\nfps: "..love.timer.getFPS(),10,10)
-	end
-end
+end end
 
 function pauseddraw()
-	love.graphics.setFont(dayposterblack) love.graphics.printf("PAUSED", 0, 200, 512, "center")
+	love.graphics.setFont(dayposterblack) love.graphics.printf("GAME PAUSED",0,200,512,"center")
 end
 
 function love.keypressed(key)
@@ -79,8 +63,7 @@ function love.keypressed(key)
 		love.draw,pauseddraw = pauseddraw,love.draw
 	elseif key==keys.quit then
 		love.event.push("quit")
-	end
-end
+end end
 
 function love.keyreleased(key)
 	if key==keys.up then
@@ -91,5 +74,4 @@ function love.keyreleased(key)
 		world.player.ax=0
 	elseif key==keys.right then
 		world.player.ax=0
-	end
-end
+end end
